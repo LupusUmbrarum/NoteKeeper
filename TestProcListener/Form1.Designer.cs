a6ffa654a8c6ead1c77a3312d45da7740807d41f
N_Cowley <cowley.nate.m9@gmail.com>
2020-04-09 14:06:39 -0500

    Minor update
    * began setting up visuals
    * began laying groundwork for how to keep track of processes with the ProcessWatcher class

---
+++ TestProcListener/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace TestProcListener
+{
+	partial class MainWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(784, 561);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.treeView1);
+			this.tabPage1.Controls.Add(this.richTextBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(776, 535);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Main Menu";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(481, 6);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(289, 523);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(265, 277);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(416, 207);
+			this.treeView1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(207, 230);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.Controls.Add(this.tabControl1);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "MainWindow";
+			this.Text = "Process Listener";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ TestProcListener/ProcessWatcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProcListener
+{
+	class ProcessWatcher
+	{
+		private static ProcessWatcher instance;
+
+		// sample process note in settings:
+		// <ProcessWatcherNote ProcName="notepad.exe" UserProcName="Notepad">All of the notes go here</ProcessWatcherNote>
+
+		// list of watched for processes by process name, not user defined name
+		private List<string> watchedProcesses = new List<string>();
+
+		private ProcessWatcher()
+		{
+
+		}
+
+		public static ProcessWatcher getInstance()
+		{
+			if(instance == null)
+			{
+				instance = new ProcessWatcher();
+			}
+
+			return instance;
+		}
+
+		public bool addToWatchedProcesses(string newProcName)
+		{
+			if(!watchedProcesses.Contains(newProcName))
+			{
+				watchedProcesses.Add(newProcName);
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool removeFromWatchedProcesses(string oldProcName)
+		{
+			if(watchedProcesses.Contains(oldProcName))
+			{
+				watchedProcesses.Remove(oldProcName);
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
